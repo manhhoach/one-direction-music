9692c9311513616a41e9bd75c8ba57d2ab1c3d6d
Manh Hoach <manhhn.nv5@gmail.com>
2025-04-10 13:06:10 +0700

    css circle history

--- src/ui/History.jsx
+++ src/ui/History.jsx
@@ -2,11 +2,13 @@ import CustomButton from './../components/CustomButton'
 
 export default function History() {
    return (
-      <div className="w-full h-full flex flex-col items-center justify-center !bg-white pt-4 relative">
-         <div className="absolute w-[300px] h-[300px] rounded-full !bg-black -z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-         <h1 className="text-3xl text-center font-times mt-10 underline">14th December 2015</h1>
-         <img width={'25%'} height={'auto'} src="/images/history.png" alt="" className="pt-10 pb-30" />
-         <div className="w-2/3 flex justify-between items-center">
+      <div className="w-full h-screen flex flex-col items-center justify-center !bg-white pt-4 relative overflow-hidden">
+         <div className='div-history top-10 absolute rounded-full w-250 h-250 !z-[-1] bg-cover bg-center'>
+
+         </div>
+         <h1 className="text-3xl relative text-center font-times mt-10 underline">14th December 2015</h1>
+         <img width={'25%'} height={'auto'} src="/images/history.png" alt="" className="pt-10 pb-30 relative" />
+         <div className="w-2/3 flex justify-between items-center relative">
             <div className="flex flex-col justify-center items-center">
                <div className="text-center font-cousine text-[2rem]">
                   One Direction's third UK single from
