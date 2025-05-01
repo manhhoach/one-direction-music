9a2f2a9d4c680fcc5b3bf5d128169b67b4432d0c
manhhoach <manhhn.nv5@gmail.com>
2025-05-01 16:11:20 +0700

    make album detail (continue)

--- src/pages/AlbumDetail.jsx
+++ src/pages/AlbumDetail.jsx
@@ -2,9 +2,8 @@ import { useParams } from "react-router-dom"
 import { getAlbumBySlug } from "../services/albumService"
 import NotFound from "../ui/NotFound"
 import { useEffect, useState } from "react"
-import FindOutMoreButton from "./../components/FindOutMoreButton"
-import SocialIcon from './../components/SocialIcon'
-import Video from "../components/Video"
+import NetworkButton from "../components/NetworkButton"
+import SocialIcon from '../components/SocialIcon'
 
 export default function AlbumDetail() {
    const { slug } = useParams()
@@ -30,30 +29,45 @@ export default function AlbumDetail() {
    if (!album) {
       return <NotFound></NotFound>
    }
-   console.log(album)
+
    return (
       <>
-         <div className='w-full relative h-screen'>
-            <img src={album.imageCover} alt="" className="absolute top-0 left-0 w-full h-full object-fit" />
-            <div className="relative text-white text-center flex justify-center flex-col items-center h-full">
-
-               <span style={{
-                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6), -2px -2px 4px rgba(0, 0, 0, 0.6)'
-               }} className="text-[12rem] uppercase font-black font-oswald">{album.name}</span>
-
+         <div className='w-full relative' style={{ minHeight: '100vh' }}>
+            <img
+               src={album.imageCover}
+               alt=""
+               className="absolute top-0 left-0 w-full h-full object-fit"
+            />
+            <p style={{
+               textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6), -2px -2px 4px rgba(0, 0, 0, 0.6)'
+            }} className="text-[12rem] text-white text-center relative uppercase font-black font-oswald pt-60">{album.name}
+            </p>
+         </div>
+         <div className="w-full h-[800px] flex ">
+            <div className="w-1/2 h-full flex items-center justify-center" style={{ backgroundColor: 'rgba(25,25,25)' }}>
+               <a className="link-style uppercase bold italic text-white text-center !text-5xl">Photos</a>
+            </div>
+            <div className="w-1/2 h-full flex flex-col items-center gap-10" style={{ backgroundColor: '#ccc' }}>
+               <p className="text-center text-xl font-times mt-5">{album.name}</p>
+               <img src={album.image} alt="" className="w-1/2 object-contain" />
+               <div className="w-full flex justify-center items-center gap-10 mt-5">
+                  {
+                     album.linksToBuy && album.linksToBuy.map((link, index) => (
+                        <NetworkButton key={link.icon} href={link.link} target="_blank">
+                           <SocialIcon size={40} icon={link.icon} />
+                        </NetworkButton>
+                     ))
+                  }
+               </div>
             </div>
          </div>
-         {/* <div className="w-full h-screen">
-            {
-               album.fanVideos?.map((video, index) => (
-                  <div key={index} className="w-1/2 inline-block h-full">
-                     <Video url={video}>
 
-                     </Video>
-                  </div>
-               ))
-            }
-         </div> */}
+         <div className="flex flex-col">
+            <p className="link-custom">SONGS</p>
+            <div>
+               
+            </div>
+         </div>
       </>
 
    )
--- src/ui/AlbumIntro.jsx
+++ src/ui/AlbumIntro.jsx
@@ -3,32 +3,40 @@ import FindOutMoreButton from './../components/FindOutMoreButton'
 
 export default function AlbumIntro({ album }) {
    return (
-      <div className='w-full relative h-screen'>
-         <img src={album.imageCover} alt="" className="absolute top-0 left-0 w-full h-full object-fit" style={{ filter: 'grayscale(100%)' }} />
+      <div className="w-full relative" style={{minHeight: '100vh'}}>
+         <img
+            src={album.imageCover}
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-fit"
+            style={{ filter: 'grayscale(100%)', zIndex: -2 }}
+         />
          <div
             className="absolute top-0 left-0 w-full h-full"
             style={{
                backgroundColor: album.mainColor,
                opacity: 0.6,
+               zIndex: -1
             }}
          ></div>
-         <div className="relative text-white text-center flex justify-start flex-col items-center h-full">
+         <div className="relative text-white text-center flex flex-col items-center justify-center py-24 px-10">
+            <h3 className="text-[6rem] font-bold font-cousine uppercase tracking-wide mt-30">
+               {album.name}
+            </h3>
 
-            <h3 className="text-8xl font-bold font-cousine uppercase tracking-widest mt-80">{album.name}</h3>
-
-            <div className='flex flex-col items-center gap-15'>
+            <div className="flex flex-col items-center gap-5 mt-10">
                <div className="font-source-code-pro text-2xl">
-                  <p className='uppercase'>Release date</p>
+                  <p className="uppercase">Release date</p>
                   <p>{formatDate(album.releaseDate)}</p>
                </div>
-               <div className='w-1/2 text-3xl font-source-code-pro'>
+
+               <div className="text-3xl font-source-code-pro max-w-4xl">
                   {album.description}
                </div>
-               <div className=''>
-                  <FindOutMoreButton href={`/music/albums/${album.slug}`}>Find out more</FindOutMoreButton>
-               </div>
-            </div>
 
+               <FindOutMoreButton href={`/music/albums/${album.slug}`}>
+                  Find out more
+               </FindOutMoreButton>
+            </div>
          </div>
       </div>
    );
