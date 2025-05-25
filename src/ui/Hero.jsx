92da437295c42cd37f9ac3950b02325fb000c586
manhhoach <manhhn.nv5@gmail.com>
2025-05-25 11:05:40 +0700

    responsive hero

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,14 +1,19 @@
 export default function Hero() {
    return (
-      <div className="w-full h-screen relative">
+      <div className="w-full h-[50vh] md:h-screen relative">
          <img
             src="/images/hero-2015-rect-med.jpg"
             alt="One Direction Band"
-            className="w-full h-full object-cover object-center"
+            className="w-full h-full object-fit object-center"
          />
-         <div className="w-full h-50 absolute bottom-30 left-1/2 transform -translate-x-1/2 flex items-center justify-center ">
-            <img src="/images/hero-2015-logo.png" alt="One Direction" className="w-full h-full object-contain" />
+
+         <div className="absolute bottom-[20%] left-1/2 transform -translate-x-1/2 w-[60%] sm:w-[60%] md:w-[60%]">
+            <img
+               src="/images/hero-2015-logo.png"
+               alt="One Direction"
+               className="w-full"
+            />
          </div>
       </div>
-   )
+   );
 }
