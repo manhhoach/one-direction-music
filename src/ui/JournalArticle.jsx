0291d1ecc0417520004dcf0c1e52a1808f0967d3
manhhoach <manhhn.nv5@gmail.com>
2025-05-25 11:54:57 +0700

    responsive journal article

--- src/ui/JournalArticle.jsx
+++ src/ui/JournalArticle.jsx
@@ -1,14 +1,27 @@
 export default function JournalArticle() {
    return (
-      <div className="w-full flex flex-col items-center justify-center !bg-white pt-5">
-         <h1 className="mt-10 text-times">23rd July 2020</h1>
-         <div className="flex flex-col items-center justify-center pb-25">
-            <span className="article-intro">#10YearsOf1D</span>
-            <div className="w-3/4 mt-15 mb-5">
-               <img src="/images/1D_Logotype_Black.jpg" alt="Logo 1D" className="w-full h-full object-contain" />
+      <div className="w-full flex flex-col items-center justify-center bg-white">
+         <h1 className="mt-10 font-times text-lg sm:text-xl md:text-2xl text-center tracking-wide">
+            23rd July 2020
+         </h1>
+
+         <div className="flex flex-col items-center justify-center pb-24">
+            <span className="font-playfair text-[3rem] sm:text-[4rem] md:text-[5.5rem] leading-none text-center">
+               #10YearsOf1D
+            </span>
+
+            <div className="w-[80%] sm:w-[70%] md:w-[60%] mt-16 mb-5">
+               <img
+                  src="/images/1D_Logotype_Black.jpg"
+                  alt="Logo 1D"
+                  className="w-full h-auto object-contain"
+               />
             </div>
-            <span className="font-source-code-pro text-2xl">Happy #10YearsOf1D Day!</span>
+
+            <span className="font-source-code-pro text-base sm:text-lg md:text-2xl">
+               Happy #10YearsOf1D Day!
+            </span>
          </div>
       </div>
-   )
+   );
 }
